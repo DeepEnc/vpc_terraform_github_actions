ee10dec2b010384d28e292601892294d559d9e33
deepenc <sunilojha525@gmail.com>
2024-12-02 22:39:11 +0545

    add the alb module config

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -14,3 +14,11 @@ module "ec2" {
     sg_id = module.sg.sg_id
     subnets = module.vpc.subnet_id
 }
+
+module "alb" {
+    source = "./modules/alb"
+    sg_id = module.sg.sg_id
+    subnets = module.vpc.subnet_id
+    vpc_id = module.vpc.vpc_id
+    instances = module.ec2.instances
+}
