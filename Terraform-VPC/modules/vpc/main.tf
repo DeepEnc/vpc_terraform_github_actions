ae15eefc3b98108df22a3d4a2d8f5a2e5d9bba7d
deepenc <sunilojha525@gmail.com>
2024-12-02 20:33:33 +0545

    add a tag to route table

--- Terraform-VPC/modules/vpc/main.tf
+++ Terraform-VPC/modules/vpc/main.tf
@@ -31,6 +31,9 @@ resource "aws_route_table" "route_table" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+    tags = {
+    "Name": "route_table"
+  }
 }
 
 resource "aws_route_table_association" "rta" {
